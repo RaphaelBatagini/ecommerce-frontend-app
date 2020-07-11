be5ab2c69dfa3778c9f19097ac385022a0fea92c
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-07-11 19:16:21 -0300

    Remove project default files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
-import './App.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
