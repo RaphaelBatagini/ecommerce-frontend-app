6398a37af43eab888f70a6fd9fd5659b8d240b05
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-07-11 15:09:38 -0300

    Create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Header = () => (
+    <header id="main-header" className="banner">
+        <div className="container">
+            <div className="row">
+            <div className="col-3 d-block d-lg-none">
+                <button className="navbar-toggler pb-3" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#mainNavbar" 
+                aria-controls="mainNavbar" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation">
+                <i className="fa fa-bars text-light"></i>
+                </button>
+            </div>
+            <div className="col-6 col-lg-2 brand-container">
+                <a className="brand" href="/">Minha Loja</a>
+            </div>
+            <div className="col-12 col-lg-8">
+            </div>
+            <div className="col-md-2">
+            </div>
+            </div>
+            <nav className="nav-primary navbar-expand-lg"></nav>
+        </div>
+    </header>
+);
+
+export default Header;
